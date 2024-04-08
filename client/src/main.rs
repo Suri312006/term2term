8894c25899962a9d5ac4cbd5b955e1e1a2273353
suri312006 <suri312006@gmail.com>
2024-04-08 10:19:13 -0700

    using environment variables to have port address consistent

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,16 +1,20 @@
+use dotenv::dotenv;
 use std::error::Error;
 
 fn main() {
-    println!("Hello, world!");
+    dotenv().ok();
 
-    let err = send_request();
+    let server_address =
+        std::env::var("SERVER_HOST_ADDRESS").expect("SERVER_HOST_ADDRESS not in .env file");
+
+    let err = send_request(server_address.clone());
     println!("{:#?}", err);
 }
 
-fn send_request() -> Result<(), Box<dyn Error>> {
+fn send_request(address: String) -> Result<(), Box<dyn Error>> {
     // let res = reqwest::blocking::get("https://localhost:6969/")?.text()?;
 
-    let body = reqwest::blocking::get("http://localhost:6969/")?.text();
+    let body = reqwest::blocking::get(address)?.text();
 
     println!("{:#?}", body);
 
--- server/cmd/t2t_server/t2t_server.go
+++ server/cmd/t2t_server/t2t_server.go
@@ -3,13 +3,33 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+	"strings"
 
+	"github.com/joho/godotenv"
 	api "github.com/suri312006/term2term/v2/internal/api"
 )
 
 func main() {
 	//TODO: create db here, and pass it into a db wrapper?
-	server := api.Server{}
-	log.Fatal(http.ListenAndServe(":6969", server))
+
+	err := godotenv.Load("../../../.env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	serverAdress := os.Getenv("SERVER_HOST_ADDRESS")
+
+	println("server adress", serverAdress)
+
+	port := strings.TrimPrefix(serverAdress, "http://localhost")
+	port = strings.TrimSuffix(port, "/")
 	
+
+	println("port adress", port)
+
+	server := api.Server{}
+	log.Fatal(http.ListenAndServe(port, server))
+
 }
