2b90a8f569cb37214d269943ec3b0d53a55e8cd1
suri312006 <suri312006@gmail.com>
2024-06-25 12:39:50 -0700

    basic clap example setup

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,22 @@
+use clap::Parser;
+// simple greeter
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    //Name of greetee
+    #[arg(short, long)]
+    name: String,
+
+    //Number of times
+    #[arg(short, long, default_value_t = 1)]
+    count: u64,
+}
+
 fn main() {
-    println!("Hello World!");
+    let args = Args::parse();
+
+    for _ in 0..args.count {
+        println!("Hello {}", args.name);
+    }
+
 }
