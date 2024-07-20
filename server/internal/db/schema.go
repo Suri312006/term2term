27b32563ae3c55d5c0c7b57694d941191d90cd23
suri312006 <suri312006@gmail.com>
2024-07-19 21:12:43 -0700

    added messaging type

--- server/internal/db/schema.go
+++ server/internal/db/schema.go
@@ -12,7 +12,7 @@ type User struct {
 	DeletedAt gorm.DeletedAt `gorm:"index"`
 
 	// PrivateId string `json:"id" gorm:"uniqueIndex"`
-	PublicId string `json:"id" gorm:"uniqueIndex"`
+	PubId string `json:"id" gorm:"uniqueIndex"`
 	Name     string `json:"name"`
 }
 
@@ -22,13 +22,32 @@ type Conversation struct {
 	UpdatedAt time.Time      `json:"-"`
 	DeletedAt gorm.DeletedAt `gorm:"index"`
 
-	Id      string `json:"id" gorm:"uniqueIndex"`
+	PubId   string `json:"id" gorm:"uniqueIndex"`
 	User1Id string `json:"user1_id"`
 	User2Id string `json:"user2_id"`
 
-	User1 User `gorm:"foreignKey:User1Id;references:PublicId"`
-	User2 User `gorm:"foreignKey:User2Id;references:PublicId"`
+	User1 User `gorm:"foreignKey:User1Id;references:PubId"`
+	User2 User `gorm:"foreignKey:User2Id;references:PubId"`
+}
 
+type Message struct {
+	ID        uint           `json:"-" gorm:"primarykey"`
+	CreatedAt time.Time      `json:"-"`
+	UpdatedAt time.Time      `json:"-"`
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+
+	PubId string `json:"id" gorm:"uniqueIndex"`
+
+	AuthorId string `json:"author"`
+	Author   User   `gorm:"foreignKey:AuthorId;references:PubId"`
+
+	RecipientId string `json:"recipient_id"`
+	Recipient   User   `gorm:"foreignKey:RecipientId;references:PubId"`
+
+	ConversationId string       `json:"convo_id"`
+	Conversation   Conversation `gorm:"foreignKey:ConversationId;references:PubId"`
+
+	Body string `json:"body"`
 }
 
 func (m Dbm) UpdateSchema() {
@@ -36,7 +55,6 @@ func (m Dbm) UpdateSchema() {
 }
 
 func (m Dbm) ResetSchema() {
-	m.db.Migrator().DropTable(&User{}, &Conversation{})
-	m.db.AutoMigrate(&User{}, &Conversation{})
-
+	m.db.Migrator().DropTable(&User{}, &Conversation{}, &Message{})
+	m.db.AutoMigrate(&User{}, &Conversation{}, &Message{})
 }
